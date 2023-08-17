8460f0d27a03a3e9f3dd3b4d5aaaeb8d2a5abc99
Fredy Velásquez <59968764+fredyvelasquezgt@users.noreply.github.com>
2023-08-16 23:42:42 -0600

    Update piSeriesSeq.c

--- piSeriesSeq.c
+++ piSeriesSeq.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main() {
+    int n = 1000; // ajustar N segun lo necesitemos
+    double pi_approx = 0.0;
+    double factor = 1.0;
+    double sum = 0.0;
+
+    for (int k = 0; k < n; k++) {
+        sum += factor / (2 * k + 1);
+        factor = -factor;
+    }
+
+    pi_approx = 4.0 * sum;
+
+    printf("Aprox de PI: %lf\n", pi_approx);
+
+    return 0;
+}
